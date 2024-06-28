7a8b2aee9825dc98f71b0df8da3ecf1d5a7f9cfa
Austin B Campbell/AussieDev <131830841+AustinCBYUi@users.noreply.github.com>
2024-06-27 17:27:09 -0700

    Styled and added classes to li elements.

--- utilities/index.js
+++ utilities/index.js
@@ -4,9 +4,9 @@ const Util = {}
 Util.getNav = async function (req, res, next) {
     let data = await invModel.getClassifications()
     let list = "<ul>"
-    list += '<li><a href="/" title="Home Page">Home</a></li>'
+    list += '<li class="navhome"><a href="/" title="Home Page">Home</a></li>'
     data.rows.forEach((row) => {
-        list += "<li>"
+        list += '<li class="navel">'
         //Don't use backticks I suppose!
         list += '<a href="/inv/type/' +
         row.classification_id + 
